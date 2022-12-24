c14b603be3e58c04a935eeca8ed48bb502f2efec
zhangdongxu <dxz070730>
2022-12-24 21:48:32 +0800

    更新了v1.1.0!

---
+++ bjxgj.py
@@ -0,0 +1,92 @@
+"""
+必备模块方便以后扩展
+"""
+
+
+# 读取配置文件
+def read_config(file='config.ini'):
+    import configparser
+    file = file
+    con = configparser.ConfigParser()
+    con.read(file, encoding='utf-8')
+    items = dict(con.items('User'))
+    return items
+
+
+# 获取一下必要的id
+def get_id(openid, headers):
+    import requests
+    get_id_requests = requests.request(
+        'post',
+        'https://a.welife001.com/getUser',
+        data='{"openid":"' + openid + '"}',
+        headers=headers
+    )
+    _id = get_id_requests.json()["currentUser"]["child_class_list"][int(read_config()['userid']) - 1]["_id"]
+    cid = get_id_requests.json()["currentUser"]["child_class_list"][int(read_config()['userid']) - 1]["cid"]
+    member_id = get_id_requests.json()["currentUser"]["child_class_list"][int(read_config()['userid']) - 1]["member_id"]
+    return _id, cid, member_id
+
+
+def get_homework(headers, member_id):
+    import requests
+    import sys
+    get_homework_requests = requests.request(
+        'get',
+        'https://a.welife001.com/info/getParent?type=-1&members=' + member_id + '&page=0&size=10&date=-1&hasMore=true&isRecent=true',
+        headers=headers
+    )
+    # print('你有' + str(len(get_homework_requests.json()["data"])) + '项作业未完成:')
+    # for subject_num in range(len(get_homework_requests.json()["data"])):
+    #     print(get_homework_requests.json()["data"][subject_num]["title"])
+    # choose = input('请输入你要抄的作业：[1-' + str(len(get_homework_requests.json()["data"])) + ']：')
+    # try:
+    #     homework_id = get_homework_requests.json()["data"][int(choose) - 1]["_id"]
+    # except IndexError:
+    #     print('看看你输入的什么？')
+    #     sys.exit(0)
+    return get_homework_requests
+
+
+def get_answer(cid, member_id, homework_id, headers):
+    import requests
+    url = requests.request(
+        'post', 'https://a.welife001.com/applet/notify/check4teacher',
+        data='''
+        {
+          "extra": 1,
+          "cid": "''' + cid + '''",
+          "cls_ts": 1671750618852,
+          "daka_day": "",
+          "member_id": "''' + member_id + '''",
+          "_id": "''' + homework_id + '''",
+          "page": 0,
+          "size": 10,
+          "trial": false
+        }
+        ''',
+        headers=headers
+    )
+    return url
+
+
+def homework_photo(headers, homework_id):
+    import requests
+    steal_homework = requests.request(
+        'post',
+        'https://a.welife001.com/applet/notify/check4teacherList',
+        headers=headers,
+        data='''
+        {
+            "_id": "''' + homework_id + '''",
+            "sortType": "shijiandaoxu",
+            "selectType": "quanbu",
+            "page": 0,
+            "size": 10,
+            "last_accId": "",
+            "isNew": "nextId",
+            "extra": 1
+        }
+        '''
+    )
+    return steal_homework
--- main.py
+++ main.py
@@ -6,130 +6,86 @@
 # @WECHAT TUBUG_CN
 
 
-import requests
-import sys
-import configparser
+import tkinter as tk
+from tkinter import ttk, INSERT, Text
+import random
+import bjxgj
 
-# 读取配置文件
-file = 'config.ini'
-con = configparser.ConfigParser()
-con.read(file,encoding='utf-8')
-items = dict(con.items('User'))
-# 读取配置文件 END
+win = tk.Tk()
 
-# openid = 'oWRkU0djwGt7sra2Ey2K3nxmGoEU'
-openid = items['openid']
+openid = bjxgj.read_config()['openid']
 
-# 这里是请求头
 headers = {
     "Content-Type": "application/json",
     "imprint": openid,
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
 }
 
-# 向小管家发请求（这里只是或许一些必要的id
-get_id = requests.request(
-    'post',
-    'https://a.welife001.com/getUser',
-    data='{"openid":"' + openid + '"}',
-    headers=headers
-)
-print('您现在是：'+str(get_id.json()["currentUser"]["child_class_list"][int(items['userid'])-1]['name']))
-_id = get_id.json()["currentUser"]["child_class_list"][int(items['userid'])-1]["_id"]
-cid = get_id.json()["currentUser"]["child_class_list"][int(items['userid'])-1]["cid"]
-member_id = get_id.json()["currentUser"]["child_class_list"][int(items['userid'])-1]["member_id"]
-# 获取必要id END
+win.title('班级小管家学霸版v1.0.0')
+# win.geometry('759x484')
+_id = bjxgj.get_id(openid=openid, headers=headers)[0]
+cid = bjxgj.get_id(openid=openid, headers=headers)[1]
+member_id = bjxgj.get_id(openid=openid, headers=headers)[2]
 
-# 这里是获取作业（如果上面的东西报错建议下面的东西就跑不了了
-get_homework = requests.request(
-    'get',
-    'https://a.welife001.com/info/getParent?type=-1&members='+member_id+'&page=0&size=10&date=-1&hasMore=true&isRecent=true',
-    headers=headers
-)
-
-print('你有'+str(len(get_homework.json()["data"]))+'项作业未完成:')
-for subject_num in range(len(get_homework.json()["data"])):
-    print(get_homework.json()["data"][subject_num]["title"])
-choose = input('请输入你要抄的作业：[1-'+str(len(get_homework.json()["data"]))+']：')
-try:
-    homework_id = get_homework.json()["data"][int(choose)-1]["_id"]
-except IndexError:
-    print('看看你输入的什么？')
-    sys.exit(0)
-# 获取作业列表 END
-# print(get_homework.json()["data"][4]["_id"])
-# 获取答案
-url = requests.request(
-    'post', 'https://a.welife001.com/applet/notify/check4teacher',
-    data='''
-    {
-      "extra": 1,
-      "cid": "'''+cid+'''",
-      "cls_ts": 1671750618852,
-      "daka_day": "",
-      "member_id": "'''+member_id+'''",
-      "_id": "'''+homework_id+'''",
-      "page": 0,
-      "size": 10,
-      "trial": false
-    }
-    ''',
-    headers=headers
-)
-
-steal_homework = requests.request(
-    'post',
-    'https://a.welife001.com/applet/notify/check4teacherList',
-    headers=headers,
-    data='''
-    {
-        "_id": "'''+homework_id+'''",
-        "sortType": "shijiandaoxu",
-        "selectType": "quanbu",
-        "page": 0,
-        "size": 10,
-        "last_accId": "",
-        "isNew": "nextId",
-        "extra": 1
-    }
-    '''
-)
+text = Text(win, font=('System', 16))
+text.grid(row=2, column=1, columnspan=3)
+cmb = ttk.Combobox(win)
+cmb.grid(row=1, column=1, columnspan=2)
+text.insert(INSERT,
+            '你有' + str(len(bjxgj.get_homework(headers=headers, member_id=member_id).json()["data"])) + '项作业未完成:\n')
+hw_list = ['请选择你要完成的学科']
+for subject_num in range(len(bjxgj.get_homework(headers=headers, member_id=member_id).json()["data"])):
+    text.insert(INSERT, bjxgj.get_homework(headers=headers, member_id=member_id).json()["data"][subject_num]["title"] +
+                '\n')
+    hw_list = hw_list + bjxgj.get_homework(headers=headers, member_id=member_id).json()["data"][subject_num][
+        "title"].split('\n')
+    cmb['value'] = hw_list
+cmb.current(0)
 
 
-'''
-curl 'https://a.welife001.com/applet/notify/check4teacherList' \
--X POST \
--H 'Host: a.welife001.com' \
--H 'Accept: */*' \
--H 'Accept-Language: zh-CN,zh-Hans;q=0.9' \
--H 'Connection: keep-alive' \
--H 'Referer: https://servicewechat.com/wx23d8d7ea22039466/1802/page-frame.html' \
--H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E217 MicroMessenger/6.8.0(0x16080000) NetType/WIFI Language/en Branch/Br_trunk MiniProgramEnv/Mac' \
--H 'Content-Length: 144' \
--H 'Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcGVuaWQiOiJvV1JrVTBkandHdDdzcmEyRXkySzNueG1Hb0VVIiwidW5pb25pZCI6Im90Z3pwdjU2T3pBcUNXVGpfSE9qTklVY09uNGciLCJwbGF0Zm9ybSI6Im1pbmkiLCJleHAiOjE2NzY3OTI4NjMwMjQsImlhdCI6MTY3MTYwODg2M30.x-afJTAo6hfaNm5x6HGDQyDiNK_AtnMu9FQ6rC-sOGw' \
--H 'imprint: oWRkU0djwGt7sra2Ey2K3nxmGoEU' \
--H 'Content-Type: application/json' \
--d '{"_id":"63a2cb999d775ec3c7d2f773","sortType":"shijiandaoxu","selectType":"quanbu","page":0,"size":10,"last_accId":"","isNew":"nextId","extra":1}' \
---proxy http://localhost:9090
-'''
+def hw_list_():
+    homework_id = bjxgj.get_homework(headers=headers, member_id=member_id).json()["data"][hw_list.index(cmb.get()) - 1][
+        "_id"]
+    get_answer = bjxgj.get_answer(cid=cid, member_id=member_id, homework_id=homework_id, headers=headers)
+    data = bjxgj.homework_photo(headers=headers, homework_id=homework_id).json()['data']
+    if get_answer.json()['data']['notify']['feedback_type'] == 24:
+        data = bjxgj.homework_photo(headers=headers, homework_id=homework_id).json()['data']
+        num = len(bjxgj.homework_photo(headers=headers, homework_id=homework_id).json()['data'])
+        chooses = random.randint(1, num)
+        for photo_num in range(len(data[int(chooses) - 1]['feedback_photo'])):
+            text.insert(INSERT, 'https://img.banjixiaoguanjia.com/' + data[int(chooses) - 1]['feedback_photo'][photo_num]+'\n')
+            # print('https://img.banjixiaoguanjia.com/' + data[int(chooses) - 1]['feedback_photo'][photo_num])
+    elif get_answer.json()['data']['notify']['feedback_type'] == -1:
+        text.delete(1.0, "end")
+        abcdefg = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+        _data = bjxgj.get_answer(cid=cid, headers=headers, homework_id=homework_id, member_id=member_id).json()['data']
+        for i in range(len(_data["notify"]["attach"]["subjects"])):
+            try:
+                for a in range(len(_data["notify"]["attach"]["subjects"][0]["details"])):
+                    if _data["notify"]["attach"]["subjects"][i]["details"][a]["right"] == 'y':
+                        text.insert(INSERT, '第' + str(i + 1) + '题选' + abcdefg[a] + '\n')
+                        # print('第' + str(i + 1) + '题选' + abcdefg[a])
+            except IndexError:
+                who_homework = 1
+                for photo_list in range(
+                        len(data[int(who_homework) - 1]['attach']['subjects'][i]['answers'])):
+                    text.insert(INSERT, 'https://img.banjixiaoguanjia.com/' +
+                                data[int(who_homework) - 1]['attach']['subjects'][i]['answers'][
+                                    photo_list] + '\n')
+                    # print('https://img.banjixiaoguanjia.com/' +
+                    #       data[int(who_homework) - 1]['attach']['subjects'][i]['answers'][
+                    #           photo_list])
+                # print(steal_homework.json()['data'][int(who_homework)-1]['attach']['subjects'][i]['answers'])
+                text.insert(INSERT, '第' + str(i + 1) + '题是大题，看链接！')
+    print(homework_id)
 
-abcd = ['A', 'B', 'C', 'D']
 
-for i in range(len(url.json()["data"]["notify"]["attach"]["subjects"])):
-    try:
-        for a in range(4):
-            if url.json()["data"]["notify"]["attach"]["subjects"][i]["details"][a]["right"] == 'y':
-                print('第' + str(i + 1) + '题选' + abcd[a])
-    except IndexError:
-        for good_baby in range(len(steal_homework.json()['data'])):
-            print(steal_homework.json()['data'][good_baby]['name'])
-        who_homework = input('请输入你要抄谁的作业[1-'+str(len(steal_homework.json()['data']))+']:')
-        for photo_list in range(len(steal_homework.json()['data'][int(who_homework)-1]['attach']['subjects'][i]['answers'])):
-            print('https://img.banjixiaoguanjia.com/'+steal_homework.json()['data'][int(who_homework) - 1]['attach']['subjects'][i]['answers'][photo_list])
-        # print(steal_homework.json()['data'][int(who_homework)-1]['attach']['subjects'][i]['answers'])
-        print('第'+str(i+1)+'题是大题，看链接！')
-# 获取答案 END
+def clean():
+    text.delete(1.0, "end")
 
-input('请按任意键退出...')
-# print(len(url.json()["data"]["notify"]["attach"]["subjects"]))
-# print(url.json()["data"]["notify"]["attach"]["subjects"][11]["details"][2]["right"])
+
+button = tk.Button(win, text='确认', command=hw_list_).grid(row=1, column=3)
+about = tk.Button(win, text='清空', command=clean).grid(row=3, column=1, padx=5, pady=5)
+exit_ = tk.Button(win, text='退出').grid(row=3, column=3, padx=5, pady=5)
+
+win.mainloop()
